ea916bfa77c5f64ba295b6b3a0dfeb99329d9e15
juzsoft2018 <45680111+juzsoft2018@users.noreply.github.com>
2018-12-08 04:42:47 +0000

    four

--- pscloud_two/models/.ipynb_checkpoints/subject-checkpoint.py
+++ pscloud_two/models/.ipynb_checkpoints/subject-checkpoint.py
@@ -7,8 +7,8 @@ class TrainingSubject(models.Model):
     _description = "客户"
 
     name = fields.Char(string='姓名')
-    lxfs = fields.Char( string='联系方式')
-    dz   = fields.Char(string='地址')
+   # lxfs = fields.Char( string='联系方式')
+    # dz   = fields.Char(string='地址')
     desc = fields.Text(string='描述')
 
 #  vim:et:si:sta:ts=4:sts=4:sw=4:tw=79:

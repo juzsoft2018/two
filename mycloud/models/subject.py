57e39625acfd756774b2ffe57499f87ec3e7cf1f
juzsoft2018 <45680111+juzsoft2018@users.noreply.github.com>
2018-12-09 13:36:44 +0000

    ok4

--- mycloud/models/subject.py
+++ mycloud/models/subject.py
@@ -5,10 +5,6 @@ from odoo import api, fields, models
 class TrainingSubject(models.Model):
     _name = 'mycloud.subject'
     _description = "客户"
-
-    name = fields.Char(string='姓名')
-   # lxfs = fields.Char( string='联系方式')
-    # dz   = fields.Char(string='地址')
+    name = fields.Char(string='姓名') 
     desc = fields.Text(string='描述')
-
 #  vim:et:si:sta:ts=4:sts=4:sw=4:tw=79:
